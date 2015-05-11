7df7fa9b85f66fa824365ed9685d5d7c57900723
Abhay Agarwal <abhay.agarwal@berkeley.edu>
2015-05-11 04:29:10 -0700

    updated spark jobs

---
+++ spark/src/main/scala/com/test/ShakespeareIngest.scala
@@ -0,0 +1,65 @@
+package com.test
+
+import com.datastax.spark.connector._
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+import java.util.{Calendar, UUID, Date}
+import scala.collection.immutable.HashSet
+
+case class Oink(kind: String, id: String, content: String, created_at: UUID, handle: String)
+
+object ShakespeareIngest {
+  //weird date hack for cassandra
+  def uuidForDate(d: Date): UUID = {
+    val NUM_100NS_INTERVALS_SINCE_UUID_EPOCH: Long = 0x01b21dd213814000L
+    val origTime = d.getTime
+    val time = origTime * 10000 + NUM_100NS_INTERVALS_SINCE_UUID_EPOCH
+    val timeLow = time &       0xffffffffL
+    val timeMid = time &   0xffff00000000L
+    val timeHi = time & 0xfff000000000000L
+    val upperLong = (timeLow << 32) | (timeMid >> 16) | (1 << 12) | (timeHi >> 48)
+    return new java.util.UUID(upperLong, 0xC000000000000000L)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf(true)
+      .set("spark.cassandra.connection.host", args.lift(0).getOrElse("localhost")) //connect to cassandra
+      .setExecutorEnv("HADOOP_CONF_DIR", "/opt/mesosphere/etc/hadoop") //connect to HDFS configuration
+    val sc = new SparkContext("local", "Shakespeare Ingest", conf)
+
+    val shakespeare = new SQLContext(sc) //creating a Row RDD from JSON file. One object per line
+      .jsonFile("hdfs://namenode1.hdfs.mesos:50071" + args.lift(1).getOrElse("/user/root/shakespeare_data.json"))
+    shakespeare.printSchema() //null, line_id, line_number, play name, speaker, speech_number, text_entry
+
+    val shakespeareRDD = shakespeare.rdd
+      .filter(!_.isNullAt(1)) //filter erroneous rows
+      .map( r => Oink( //create Oink object
+        "oink",
+        r.getLong(1).hashCode.toString, //random id
+        r.getString(6).substring(0, Math.min(r.getString(6).length,140)), //max 140 characters
+        uuidForDate(Calendar.getInstance.getTime),
+        r.getString(4).replaceAll("[^a-zA-Z ]","") //speaker name
+      ))
+      .cache //save for multiple operations
+
+    //get speakers as hashset
+    val authorSet = new HashSet() ++ shakespeareRDD
+      .map(_.handle)
+      .distinct
+      .toLocalIterator
+
+    //add @mentions
+    val modifiedLines = shakespeareRDD map { line =>
+      var text = line.content
+      authorSet foreach { author => //find author name in content
+        text = line.content.replace(author, "@"+author.toLowerCase.replace(" ", "_")) //add underscores in handles
+        text = text.substring(0, Math.min(text.length, 140)) //trim in case we exceed the 140 char limit
+      }
+      val author = line.handle.toLowerCase.replace(" ", "_") //underscores in handles
+      Oink(line.kind, line.id, text, line.created_at, author)
+    }
+
+    //save to cassandra
+    modifiedLines.saveToCassandra("oinker", "oinks", SomeColumns("kind", "id", "content", "created_at", "handle"))
+  }
+}
--- spark/src/main/scala/com/test/SparkCassandraTest.scala
+++ spark/src/main/scala/com/test/SparkCassandraTest.scala
@@ -1,19 +1,37 @@
 package com.test
 
-/**
- * Created by abhay on 4/27/15.
- */
 import com.datastax.spark.connector._
 import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.SparkContext._
+import scala.collection.immutable.HashSet
+
+case class Frequency(kind: String, key: String, frequency: Int)
 
 object SparkCassandraTest {
+  //A stoplist is a list of connector words that are analytically unimportant
+  val stoplist = new HashSet() ++ "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be," +
+    "because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have," +
+    "he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my," +
+    "neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than," +
+    "that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which," +
+    "while,who,whom,why,will,with,would,yet,you,your".split(",")
+
   def main(args: Array[String]) {
-    val conf = new SparkConf(true).set("spark.cassandra.connection.host", args.headOption.getOrElse("localhost"))
-    val sc = new SparkContext("local", "Oinker Analytics", conf)
-    val rdd = sc.cassandraTable("oinker", "oinks")
-    val words = rdd flatMap {_.getString("content").replaceAll("[.?!,:'\"]","").split(" ")}
-    val freqs = words.map(w => (w, 1)).reduceByKey(_ + _)
-    freqs.saveToCassandra("oinker", "analytics", SomeColumns("key", "frequency"))
+    //create a spark configuration to connect to cassandra
+    val conf = new SparkConf(true).set("spark.cassandra.connection.host", args.lift(0).getOrElse("localhost"))
+    //setup a spark job and create an RDD from a cassandra table
+    val rdd = new SparkContext("local", "Oinker Analytics", conf)
+      .cassandraTable("oinker", "oinks")
+    //turn every 'oink' into a series of words
+    val words = rdd.flatMap(o =>
+      o.getString("content") //using the 'content' field
+        .replaceAll("[^a-zA-Z ]", "") //only letters and spaces
+        .toLowerCase
+        .split(" ")) //splitting on spaces
+    val freqs = words
+      .map(w => (w, 1)) //identity
+      .reduceByKey(_ + _) //adding frequency
+      .filter(t => !stoplist.contains(t._1)) //remove items in stoplist
+      .map(t => new Frequency("oink", t._1, t._2))
+    freqs.saveToCassandra("oinker", "analytics", SomeColumns("kind", "key", "frequency"))
   }
 }
