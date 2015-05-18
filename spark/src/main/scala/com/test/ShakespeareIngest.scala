8492df7aeddd6482ad60effbeec8c5c1c5c11e1d
Abhay Agarwal <abhay.agarwal@berkeley.edu>
2015-05-18 16:58:50 -0700

    shakespeare uses HDFS

--- spark/src/main/scala/com/test/ShakespeareIngest.scala
+++ spark/src/main/scala/com/test/ShakespeareIngest.scala
@@ -4,7 +4,6 @@ import com.datastax.spark.connector._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 import java.util.{Calendar, UUID, Date}
-import scala.collection.immutable.HashSet
 
 case class Oink(kind: String, id: String, content: String, created_at: UUID, handle: String)
 
@@ -24,11 +23,10 @@ object ShakespeareIngest {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf(true)
       .set("spark.cassandra.connection.host", args.lift(0).getOrElse("localhost")) //connect to cassandra
-      .setExecutorEnv("HADOOP_CONF_DIR", "/opt/mesosphere/etc/hadoop") //connect to HDFS configuration
     val sc = new SparkContext("local", "Shakespeare Ingest", conf)
 
     val shakespeare = new SQLContext(sc) //creating a Row RDD from JSON file. One object per line
-      .jsonFile("hdfs://namenode1.hdfs.mesos:50071" + args.lift(1).getOrElse("/user/root/shakespeare_data.json"))
+      .jsonFile("hdfs://hdfs/" + args.lift(1).getOrElse("/user/root/shakespeare_data.json"))
     shakespeare.printSchema() //null, line_id, line_number, play name, speaker, speech_number, text_entry
 
     val shakespeareRDD = shakespeare.rdd
@@ -40,26 +38,7 @@ object ShakespeareIngest {
         uuidForDate(Calendar.getInstance.getTime),
         r.getString(4).replaceAll("[^a-zA-Z ]","") //speaker name
       ))
-      .cache //save for multiple operations
 
-    //get speakers as hashset
-    val authorSet = new HashSet() ++ shakespeareRDD
-      .map(_.handle)
-      .distinct
-      .toLocalIterator
-
-    //add @mentions
-    val modifiedLines = shakespeareRDD map { line =>
-      var text = line.content
-      authorSet foreach { author => //find author name in content
-        text = line.content.replaceAllLiterally(author, "@"+author.toLowerCase.replace(" ", "_")) //add underscores
-        text = text.substring(0, Math.min(text.length, 140)) //trim in case we exceed the 140 char limit
-      }
-      val author = line.handle.toLowerCase.replace(" ", "_") //underscores in handles
-      Oink(line.kind, line.id, text, line.created_at, author)
-    }
-
-    //save to cassandra
-    modifiedLines.saveToCassandra("oinker", "oinks", SomeColumns("kind", "id", "content", "created_at", "handle"))
+    shakespeareRDD.saveToCassandra("oinker", "oinks", SomeColumns("kind", "id", "content", "created_at", "handle"))
   }
 }
