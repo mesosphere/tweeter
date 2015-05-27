62fddc64e15df6bf7e5bd5a06678affac55ffffa
Sebastien Pahl <sebastien@mesosphere.io>
2015-05-27 16:03:15 -0700

    Remove trailing / from proxy_pass target

--- router/app.lua
+++ router/app.lua
@@ -4,4 +4,4 @@ resp = ngx.location.capture('/__mesos_dns/v1/services/_oinker._tcp.marathon.meso
 
 backends = cjson.decode(resp.body)
 backend = backends[1]
-ngx.var.target = "http://" .. backend['ip'] .. ":"  .. backend['port'] .. "/"
+ngx.var.target = "http://" .. backend['ip'] .. ":"  .. backend['port']
